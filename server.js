be801886a8a70a0f85c94cb9ece12ab01d3a54f5
Suryakant-gupta <suryakantgupta678.com>
2024-09-06 11:43:09 +0530

    checking location based phone number functionality

--- server.js
+++ server.js
@@ -2296,7 +2296,7 @@ app.post('/get-phone-number', (req, res) => {
   
     // Simple check for India based on latitude and longitude
     if (latitude >= 8.4 && latitude <= 37.6 && longitude >= 68.7 && longitude <= 97.25) {
-      res.json({ phone: '+91-7084992604' });  // Indian phone number
+      res.json({ phone: '18005710605' });  // Indian phone number
     } else {
       res.json({ phone: '+1-321-125-5890' });  // Non-Indian phone number
     }
